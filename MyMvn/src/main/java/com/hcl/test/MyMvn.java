71527791894723951de83e7fc5c490ae55834634
naveenchandra.j <naveenchandra.j@hcl.com>
2015-11-26 17:42:11 +0530

    first commit.

---
+++ MyMvn/src/main/java/com/hcl/test/MyMvn.java
@@ -0,0 +1,14 @@
+package com.hcl.test;
+
+public class MyMvn {
+
+	public MyMvn() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+
+}
